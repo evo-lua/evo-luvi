7e202bf544ec70d2a509aeea796407ac1a7c51b1
RDW <duckwhale@mail.ru>
2021-10-17 11:52:04 +0200

    Tests: Fix error in test for path.resolve on *nix

--- test/extensions/path/test-path-resolve.lua
+++ test/extensions/path/test-path-resolve.lua
@@ -4,21 +4,32 @@ local ffi = require("ffi")
 local uv = require("uv")
 
 -- JavaScript is truly a thing of beauty...
-local posixyCwd = ffi.os == "Windows" and
- (function()
-    local _ = uv.cwd():gsub(path.separator, path.posix.separator)
+local uv = require("uv")
+local posixyCwd = (ffi.os == "Windows") and
+ ((function()
+	-- cwd will have backslashes, but the posix version must always return slashes (even on Windows systems)
+    local _ = uv.cwd():gsub(path.win32.separator, path.posix.separator)
 	local posixPath = _:sub(_:find(path.posix.separator), #_)
     return posixPath
- end)()
-  or
-  process.cwd()
+end)())
+or
+  uv.cwd()
 
-local uv = require("uv")
+local windowsCwd = (function()
+	local cwd = uv.cwd()
+	if ffi.os ~= "Windows" then
+		-- cwd will have slashes, but the win32 version must always return backslashes (even on Unixlike systems)
+		return cwd:gsub("/", "\\")
+	end
+	return cwd
+end)()
+
 
 local function getCurrentDeviceRoot()
 	local cwd = uv.cwd()
 	-- It's always an absolute path, which on windows starts with a disk designator (letter and colon)
-	return cwd:sub(1, 2)
+	-- Resolving win32 paths doesn't make much sense on Unix systems, but this does reflect the behavior of node on Linux at least...
+	return (ffi.os == "Windows") and cwd:sub(1, 2) or ""
 end
 
 local windowsTestCases = {
@@ -27,7 +38,7 @@ local windowsTestCases = {
      {{'c:/ignore', 'd:\\a/b\\c/d', '\\e.exe'}, 'd:\\e.exe'}, -- d is the last drive visited, so stay on there. network paths do not change the current drive
      {{'c:/ignore', 'c:/some/file'}, 'c:\\some\\file'}, -- cd in same drive means the second command overrides the first
      {{'d:/ignore', 'd:some/dir//'}, 'd:\\ignore\\some\\dir'}, -- d: is invalid drive identifier, so it should be skipped
-     {{'.'}, uv.cwd()}, -- cwd is resolved properly
+     {{'.'}, windowsCwd}, -- cwd is resolved properly
      {{'//server/share', '..', 'relative\\'}, '\\\\server\\share\\relative'},
      {{'c:/', '//'}, 'c:\\'},
      {{'c:/', '//dir'}, 'c:\\dir'},
@@ -37,7 +48,7 @@ local windowsTestCases = {
      {{'C:\\foo\\tmp.3\\', '..\\tmp.3\\cycles\\root.js'},
       'C:\\foo\\tmp.3\\cycles\\root.js'},
 	  -- Custom tests (since the NodeJS ones don't seem to exercise all code paths, for some reason)
-	  { {"ignore/dir"}, uv.cwd() .. "\\ignore\\dir" }, -- relative path resolution should use the current drive's cwd
+	  { {"ignore/dir"}, windowsCwd .. "\\ignore\\dir" }, -- relative path resolution should use the current drive's cwd
 	  { {"ignore", "", "/dir"}, getCurrentDeviceRoot() .. "\\dir" } -- empty path segments should be ignored
 }
 
@@ -45,9 +56,10 @@ local posixTestCases = {
     -- Arguments                    result
     {{'/var/lib', '../', 'file/'}, '/var/file'},
      {{'/var/lib', '/../', 'file/'}, '/file'},
-     {{'a/b/c/', '../../..'}, posixyCwd}, -- TBD: Use WSL-style path resolution for cwd instead?
+     {{'a/b/c/', '../../..'}, posixyCwd},
      {{'.'}, posixyCwd},
      {{'/some/dir', '.', '/absolute/'}, '/absolute'},
+	 { {"ignore", "", "/dir"}, "/dir" }, -- empty path segments should be ignored
      {{'/foo/tmp.3/', '../tmp.3/cycles/root.js'}, '/foo/tmp.3/cycles/root.js'},
 }
 
