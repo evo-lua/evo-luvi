176d20f8f5de0cef414fa2fca172e08a5b4a6e06
RDW <duckwhale@mail.ru>
2021-10-19 03:49:14 +0200

    Lua: Add new primitive for the virtual file system
    This is mostly for convenience/because I'm lazy, but it might be useful to abstract some low-level internals of Luvi's bundling functionality for future extension.
    
    Note: I haven't added tests for the functionality itself since it's directly using luvi.bundle, which has its own tests.

--- src/lua/primitives.lua
+++ src/lua/primitives.lua
@@ -4,6 +4,7 @@ package.path = package.path .. ";primitives/?.lua;"
 -- Primitives (need to be loaded first, as libaries may depend on them)
 local primitives = {
 	["v8_string_helpers"] = require("v8_string_helpers"),
+	["virtual_file_system"] = require("virtual_file_system"),
 }
 
 return primitives
--- test/test-primitives-are-available.lua
+++ test/test-primitives-are-available.lua
@@ -1,5 +1,8 @@
 -- Primitives should be returned immediately from the require cache as they must be preloaded
 print(package.preload["v8_string_helpers"])
+print(package.preload["virtual_file_system"])
 local v8_string_helpers = require("v8_string_helpers")
+local virtual_file_system = require("virtual_file_system")
 
 assertStrictEqual(type(v8_string_helpers), "table", "The V8 string helpers should be loaded")
+assertStrictEqual(type(virtual_file_system), "table", "The virtual file system primitives should be loaded")
