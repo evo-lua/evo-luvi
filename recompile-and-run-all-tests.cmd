393975120e368673016871217d3b4814caeac9f0
RDW <duckwhale@mail.ru>
2021-10-19 09:24:13 +0200

    CI: Fix batch script not returning when encountering errors
    The nested calls will return with an error code, but the top-level script won't recognize them. This leads to the CI failing to detect errors during the build verification phase.

--- recompile-and-run-all-tests.cmd
+++ recompile-and-run-all-tests.cmd
@@ -2,12 +2,28 @@
 REM You may need to run this from a Visual Studio prompt if using MSVC (no idea about gcc)
 REM Example: x64 Native Tools Command Prompt for VS <version>
 
-REM Since luvi won't pass the CLI arguments before the "--" delimiter to the bundled app,
-REM they have to be passed manually for now (yes, it's ugly)
-CALL make.bat
+REM This should set up the build environment for standard builds the first time it's run
+CALL make.bat regular
+IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+
+CALL make.bat test
+IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+
+REM The extension test suite is a Luvi app we can simply run after the build
 luvi.exe test
+IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+
+REM The tests for the import extension are implemented as a separate Luvi app and need to be run from multiple directories
 cd test/extensions/import
+
 CALL test-import-from-cwd.cmd
+IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+
 CALL test-import-from-another-dir.cmd
+IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+
 CALL test-import-from-compiled-bundle.cmd
+IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+
+REM This is just added for convenience when repeatedly calling the script via CLI
 cd ../../..
