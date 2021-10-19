169cb3def8312c465bf68962c96aae814ff537a2
RDW <duckwhale@mail.ru>
2021-10-19 04:57:34 +0200

    Test: Add basic smoke test for the import extension

--- test/test-extensions-are-loaded.lua
+++ test/test-extensions-are-loaded.lua
@@ -1,2 +1,3 @@
 assertStrictEqual(type(_G.path), "table", "The global path library should be loaded")
 assertStrictEqual(type(_G.dump), "function", "The global dump utility should be loaded")
+assertStrictEqual(type(_G.import), "function", "The global import utility should be loaded")
