e1b0c818719db7e18277311d010ac8b8ab9f1456
RDW <duckwhale@mail.ru>
2021-10-20 02:55:09 +0200

    Lua: Replace v8_string_helpers primitive with v8_string
    This is just to get rid off the deprecated name in the codebase, so that removing the primitive later will require zero additional work.

--- test/test-primitives-are-available.lua
+++ test/test-primitives-are-available.lua
@@ -1,9 +1,9 @@
-local v8_string_helpers = require("v8_string_helpers")
-local virtual_file_system = require("virtual_file_system")
+local v8_string = require("v8_string")
+local vfs = require("virtual_file_system")
 
 -- Primitives should be returned immediately from the require cache as they must be preloaded
-assertStrictEqual(package.preload["v8_string_helpers"], v8_string_helpers, "v8_string_helpers primitive should be preloaded")
-assertStrictEqual(package.preload["virtual_file_system"], virtual_file_system, "virtual_file_system primitive should be preloaded")
+assertStrictEqual(package.preload["v8_string"], v8_string, "Should preload the v8_string primitive")
+assertStrictEqual(package.preload["virtual_file_system"], vfs, "Should preload the virtual_file_system primitive")
 
-assertStrictEqual(type(v8_string_helpers), "table", "The V8 string helpers should be loaded")
-assertStrictEqual(type(virtual_file_system), "table", "The virtual file system primitives should be loaded")
+assertStrictEqual(type(v8_string), "table", "Should be able to require the v8_string primitive")
+assertStrictEqual(type(vfs), "table", "Should be able to require the virtual_file_system primitive")
