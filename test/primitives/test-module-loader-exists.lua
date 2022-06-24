312ed77ec357f958e7ab13f84edbbc301b9849ac
RDW <duckwhale@mail.ru>
2022-06-24 08:44:45 +0200

    Update test-module-loader-exists.lua

--- test/primitives/test-module-loader-exists.lua
+++ test/primitives/test-module-loader-exists.lua
@@ -9,7 +9,35 @@ assertStrictEqual(#ModuleLoader.moduleCache, 0, "Should initialize an empty modu
 assertStrictEqual(#ModuleLoader.prefixStack, 0, "Should initialize an empty prefix stack")
 
 assertStrictEqual(type(ModuleLoader.ImportModule), "function", "Should export function ImportModule")
--- assertStrictEqual(type(ModuleLoader.GetScriptRoot), "function", "ModuleLoader.GetScriptRoot is a function")
+
+local nonStringValues = {
+	["number"] = 42,
+	["table"] = {},
+	["function"] = print,
+	["nil"] = nil,
+}
+local expectedUsageError = "Usage: import(modulePath)"
+for valueType, value in pairs(nonStringValues) do
+	local success, errorMessage = ModuleLoader:ImportModule(value)
+	assertStrictEqual(success, nil, "Should fail if the given module path is a " .. valueType)
+	assertStrictEqual(errorMessage, expectedUsageError, "Should return an error if the given path is a " .. valueType)
+end
+
+local invalidStringValues = {
+	"",
+	"@",
+}
+for _, value in pairs(invalidStringValues) do
+	local success, errorMessage = ModuleLoader:ImportModule(value)
+	assertStrictEqual(success, nil, "Should fail if the given module path is " .. value)
+	assertStrictEqual(errorMessage, expectedUsageError, "Should return an error if the given path is " .. value)
+end
+
+
+-- assertStrictEqual(ModuleLoader:ImportModule(), "", "Should fail if the given module path is an empty string")
+
+
+	-- assertStrictEqual(type(ModuleLoader.GetScriptRoot), "function", "ModuleLoader.GetScriptRoot is a function")
 -- assertStrictEqual(type(ModuleLoader.GetCurrentScript), "function", "ModuleLoader.GetCurrentScript is a function")
 -- assertStrictEqual(type(ModuleLoader.GetModuleCache), "function", "ModuleLoader.GetModuleCache is a function")
 -- assertStrictEqual(type(ModuleLoader.GetPrefixStack), "function", "ModuleLoader.GetPrefixStack is a function")
