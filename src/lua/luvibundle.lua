0411fa33007e7c8414ad61735256bac64e2325ea
RDW <duckwhale@mail.ru>
2022-06-28 14:16:33 +0200

    Lua: Add annotations to untested code that must not be changed
    These can't be refactored without tests, but they can't be tested easily without refactoring... Will have to decide how to deal with them on a case-by-case basis, but not right now due to the risk of introducing regressions.

--- src/lua/luvibundle.lua
+++ src/lua/luvibundle.lua
@@ -304,12 +304,12 @@ local function commonBundle(bundlePaths, mainPath, args)
   mainPath = mainPath or "main.lua"
 
   local bundle = assert(makeBundle(bundlePaths))
-  luvi.bundle = bundle
+  luvi.bundle = bundle -- untested
 
   bundle.paths = bundlePaths
   bundle.mainPath = mainPath
 
-  function bundle.action(path, action, ...)
+  function bundle.action(path, action, ...) -- untested
     -- If it's a real path, run it directly.
     if uv.fs_access(path, "r") then return action(path) end
     -- Otherwise, copy to a temporary folder and run from there
@@ -328,7 +328,7 @@ local function commonBundle(bundlePaths, mainPath, args)
   end
 
 
-local function exportScriptGlobals()
+local function exportScriptGlobals() -- untested
 	local cwd = uv.cwd()
 
 	_G.DEFAULT_USER_SCRIPT_ENTRY_POINT = "main.lua"
@@ -343,7 +343,7 @@ local function exportScriptGlobals()
 	_G.USER_SCRIPT_ROOT = scriptRoot
 end
 
-  function bundle.register(name, path)
+  function bundle.register(name, path) -- untested
     if not path then path = name + ".lua" end
     package.preload[name] = function (...)
       local lua = assert(bundle.readfile(path))
@@ -382,6 +382,7 @@ end
   if mainRequire then
     return mainRequire("./" .. mainPath)
   else
+	-- untested
     local main = bundle.readfile(mainPath)
     if not main then error("Missing " .. mainPath .. " in " .. bundle.base) end
     local fn = assert(loadstring(main, "bundle:" .. mainPath))
