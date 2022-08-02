c7e63f4ea0d487d085c9f52e56f49e5088904eb4
RDW <duckwhale@mail.ru>
2022-08-02 14:39:33 +0200

    Runtime: Add an extension for the uv builtin
    This was already part of the evo prototype, but I forgot to add it when adapting the Networking API.
    
    The reason this is added in Lua, with hardcoded error constants, is that the original libuv function isn't exposed (from luv), and that it may leak memory (which is to be avoided).

---
+++ Tests/Extensions/uv.spec.lua
@@ -0,0 +1,33 @@
+describe("Extensions: uv library", function()
+	describe("errors", function()
+		it("should export human-readable error messages for all libuv error constants", function()
+			local expectedErrorConstants = {
+				"ECONNRESET",
+				"EADDRINUSE",
+			}
+
+			local uv = require("uv")
+			local expectedNumErrorConstants = 80 -- Not great to hardcode this, as it will probably change in the future?
+
+			assertEquals(table.count(uv.errors), expectedNumErrorConstants)
+
+			for _, errorConstant in ipairs(expectedErrorConstants) do
+				assertEquals(type(errorConstant), "string")
+
+				local friendlyErrorMessage = uv.errors[errorConstant]
+				assertEquals(type(friendlyErrorMessage), "string")
+			end
+		end)
+	end)
+
+	describe("strerror", function()
+		local uv = require("uv")
+		it("should return UV_UNKNOWN if an invalid error code was passed", function()
+			assertEquals(uv.strerror(12345), uv.errors.UNKNOWN)
+		end)
+
+		it("should return the libuv error string if a valid error code was passed", function()
+			assertEquals(uv.strerror("ECONNRESET"), "Connection reset by peer")
+		end)
+	end)
+end)
