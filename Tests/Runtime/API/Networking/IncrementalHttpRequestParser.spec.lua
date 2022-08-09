5572efa7fc838d47fcc1d4edca60062bba9b62ad
RDW <duckwhale@mail.ru>
2022-08-09 22:24:26 +0200

    Update IncrementalHttpRequestParser.spec.lua

--- Tests/Runtime/API/Networking/IncrementalHttpRequestParser.spec.lua
+++ Tests/Runtime/API/Networking/IncrementalHttpRequestParser.spec.lua
@@ -1,6 +1,17 @@
 local HttpRequest = C_Networking.HttpRequest
 local IncrementalHttpRequestParser = C_Networking.IncrementalHttpRequestParser
 
+local helloWorldRequest = {
+	method = "GET",
+	requestedURL = "/hello-world.html",
+	versionString = "HTTP/1.1",
+	headers = {
+		["Host"] = "example.com:8000",
+		[1] = "Host",
+	},
+	body = {},
+}
+
 local websocketsUpgradeRequest = {
 	method = "GET",
 	requestedURL = "/chat",
@@ -20,6 +31,13 @@ local websocketsUpgradeRequest = {
 	body = {},
 }
 
+local helloWorldRequestString = "GET /hello-world.html HTTP/1.1\r\nHost: example.com:8000\r\n\r\n"
+
+local helloWorldRequestStrings = {
+	"GET /hello-world.html HTTP/1.1\r\n",
+	"Host: example.com:8000\r\n\r\n",
+}
+
 local websocketsRequestString =
 	"GET /chat HTTP/1.1\r\nHost: example.com:8000\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\nSec-WebSocket-Version: 13\r\n\r\n"
 
@@ -37,27 +55,28 @@ describe("IncrementalHttpRequestParser", function()
 	end)
 
 	describe("ParseNextChunk", function()
-		local parser = IncrementalHttpRequestParser()
 		it("should update the buffered request if a valid HTTP message was parsed in a single chunk", function()
-			assert(parser:ParseNextChunk(websocketsRequestString))
-			assert(parser:FinalizeBufferedRequest())
-			-- parser:FinalizeBufferedRequest()
+			local parser = IncrementalHttpRequestParser()
+			parser:ParseNextChunk(helloWorldRequestString)
 			dump(parser)
-			assertEquals(parser:GetBufferedRequest(), websocketsUpgradeRequest)
+			parser:FinalizeBufferedRequest()
+			dump(parser)
+			assertEquals(parser:GetBufferedRequest(), helloWorldRequest)
 		end)
 
 		it("should update the buffered request if a valid HTTP message was parsed in multiple chunks", function()
+			local parser = IncrementalHttpRequestParser()
 			local incompleteRequest = HttpRequest()
 			incompleteRequest.method = "GET"
 			incompleteRequest.versionString = "HTTP/1.1"
 			incompleteRequest.requestedURL = "/chat"
 
-			assert(parser:ParseNextChunk(websocketsRequestStrings[1]))
+			parser:ParseNextChunk(helloWorldRequestStrings[1])
 			assertEquals(parser:GetBufferedRequest(), incompleteRequest)
-			assert(parser:ParseNextChunk(websocketsRequestStrings[2]))
-			assert(parser:FinalizeBufferedRequest())
+			parser:ParseNextChunk(helloWorldRequestStrings[2])
+			parser:FinalizeBufferedRequest()
 			dump(parser)
-			assertEquals(parser:GetBufferedRequest(), websocketsUpgradeRequest)
+			assertEquals(parser:GetBufferedRequest(), helloWorldRequest)
 		end)
 	end)
 
@@ -66,7 +85,7 @@ describe("IncrementalHttpRequestParser", function()
 	describe("ResetInternalState", function()
 		it("should re-initialize the parser with an empty request cache", function()
 			local parser = IncrementalHttpRequestParser()
-			parser:ParseNextChunk(websocketsRequestString)
+			parser:ParseNextChunk(helloWorldRequestString)
 			-- parser:ResetInternalState()
 			assertEquals(parser:GetBufferedRequest(), nil)
 		end)
