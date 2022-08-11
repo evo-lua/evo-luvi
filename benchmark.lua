c34f50c7a7fa1c0596a79562dd3dfbb988e98e66
RDW <duckwhale@mail.ru>
2022-08-11 03:26:19 +0200

    wip benchmarks

---
+++ benchmark.lua
@@ -0,0 +1,9 @@
+local benchmarks = {
+	"./Benchmarks/llhttp-ffi.throughput.lua",
+}
+
+for _, filePath in pairs(benchmarks) do
+	local benchmark = import(filePath)
+	-- For CI pipelines and scripts, ensure the return code indicates EXIT_FAILURE if at least one assertion has failed
+	assert(benchmark:Start(), "Assertion failure in benchmark " .. filePath)
+end
