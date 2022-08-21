87f67374364f912ac8bbc86ee98d7f66e874851b
RDW <duckwhale@mail.ru>
2022-08-21 23:27:49 +0200

    Update Executable.lua

--- BuildScripts/Ninja/Executable.lua
+++ BuildScripts/Ninja/Executable.lua
@@ -1,3 +1,34 @@
-local Executable = {}
+local ffi = require("ffi")
+
+local BuildTargetMixin = import("./BuildTargetMixin.lua")
+local NinjaFile = import("../Ninja/NinjaFile.lua")
+
+local Executable = {
+	fileExtension = (ffi.os == "Windows") and "exe" or ""
+}
+
+function Executable:Construct(name)
+	local instance = {
+		includeDirectories = {},
+		sources = {},
+		name = name,
+	}
+
+	instance.__index = self
+	setmetatable(instance, instance)
+
+	return instance
+end
+
+Executable.__call = Executable.Construct
+setmetatable(Executable, Executable)
+
+mixin(Executable, BuildTargetMixin)
+
+function Executable:CreateBuildFile()
+	local ninjaFile = NinjaFile()
+
+	return ninjaFile
+end
 
 return Executable
