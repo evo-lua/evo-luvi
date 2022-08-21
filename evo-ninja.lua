fe390c6a412e5f2df186fa42d37cd22a31c6e5a8
RDW <duckwhale@mail.ru>
2022-08-21 14:34:43 +0200

    Update evo-ninja.lua

--- evo-ninja.lua
+++ evo-ninja.lua
@@ -1,4 +1,20 @@
-local llhttp = import("./BuildScripts/Targets/llhttp.lua")
+local NINJA_BUILD_DIR = "ninjabuild"
 
-local llhttpBuildFile = llhttp:CreateBuildFile()
-llhttpBuildFile:Save("llhttp.ninja")
+local path_join = path.join
+
+local externalBuildTargets = {
+	"llhttp",
+}
+
+print("Processing build configuration for external targets...")
+
+for index, targetID in ipairs(externalBuildTargets) do
+	printf("Creating build file for target: %s", targetID)
+
+	local target = import("./BuildScripts/Targets/" .. targetID)
+	local buildFile = target:CreateBuildFile()
+
+	local outputFilePath = path_join(NINJA_BUILD_DIR, targetID .. ".ninja")
+	buildFile:Save(outputFilePath)
+	printf("-> Configuration saved as %s", outputFilePath)
+end
