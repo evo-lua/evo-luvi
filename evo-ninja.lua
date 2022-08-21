bebe1570af87f566a5186f6cf9797f9f9fef5c1a
RDW <duckwhale@mail.ru>
2022-08-21 23:27:56 +0200

    Update evo-ninja.lua

--- evo-ninja.lua
+++ evo-ninja.lua
@@ -1,4 +1,5 @@
 local NINJA_BUILD_DIR = "ninjabuild"
+local DEFAULT_BUILD_FILE = "build.ninja"
 
 local path_join = path.join
 
@@ -6,17 +7,28 @@ local externalBuildTargets = {
 	"llhttp",
 }
 
-print("Processing build configuration for external targets...")
+print("Processing build configuration for external projects...")
 
-for index, targetID in ipairs(externalBuildTargets) do
+local function CreateNinjaBuildFile(targetID)
 	printf("Creating build file for target: %s", targetID)
 
 	local target = import("./BuildScripts/Targets/" .. targetID)
 	local buildFile = target:CreateBuildFile()
 
 	local outputFilePath = path_join(NINJA_BUILD_DIR, targetID .. ".ninja")
+	if targetID == "evo" then
+		-- This is the name ninja will default to, so let's use it to make building more convenient
+		outputFilePath = path_join(NINJA_BUILD_DIR, DEFAULT_BUILD_FILE)
+	end
 	buildFile:Save(outputFilePath)
 	printf("-> Configuration saved as %s", outputFilePath)
 end
+
+for index, targetID in ipairs(externalBuildTargets) do
+	CreateNinjaBuildFile(targetID)
+end
+
+print("Processing build configuration...")
+CreateNinjaBuildFile("evo")
 
 printf("All done! To build the runtime, type %s", transform.green("ninja -C ninjabuild"))
