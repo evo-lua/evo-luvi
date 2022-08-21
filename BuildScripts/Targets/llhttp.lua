b362a60c02b87b5e043ecc7df6d1740040e99de9
RDW <duckwhale@mail.ru>
2022-08-21 14:17:56 +0200

    Update llhttp.lua

--- BuildScripts/Targets/llhttp.lua
+++ BuildScripts/Targets/llhttp.lua
@@ -4,14 +4,14 @@ local StaticLibrary = import("../Ninja/StaticLibrary.lua")
 
 local llhttp = StaticLibrary("llhttp")
 
-local rootDirectory = path.join("..", "deps", "llhttp-ffi", "llhttp")
+local rootDirectory = path.join("deps", "llhttp-ffi", "llhttp")
 local includeDirectory = path.join(rootDirectory, "include")
 
 local sources = {
-	path_join("..", "deps", "llhttp-ffi", "llhttp", "src", "api.c"),
-	path_join("..", "deps", "llhttp-ffi", "llhttp", "src", "http.c"),
-	path_join("..", "deps", "llhttp-ffi", "llhttp", "src", "llhttp.c"),
-	-- path_join("..", "deps", "llhttp-ffi", "llhttp", "Makefile"),
+	path_join("deps", "llhttp-ffi", "llhttp", "src", "api.c"),
+	path_join("deps", "llhttp-ffi", "llhttp", "src", "http.c"),
+	path_join("deps", "llhttp-ffi", "llhttp", "src", "llhttp.c"),
+	-- path_join("deps", "llhttp-ffi", "llhttp", "Makefile"),
 }
 
 llhttp:AddIncludeDirectory(includeDirectory)
