b6be8eaba36275382eeaed5b3dc6fafc29c602a1
RDW <duckwhale@mail.ru>
2022-08-21 23:27:41 +0200

    Create BuildTargetMixin.lua

---
+++ BuildScripts/Ninja/BuildTargetMixin.lua
@@ -0,0 +1,26 @@
+local BuildTargetMixin = {
+	includeDirectories = {},
+	sources = {},
+}
+
+local GCC_INCLUDE_FLAG = "-I"
+
+function BuildTargetMixin:AddIncludeDirectory(directoryPath)
+	self.includeDirectories[#self.includeDirectories+1] = directoryPath
+end
+
+function BuildTargetMixin:AddFiles(sourceFilePaths)
+	for _, sourceFilePath in ipairs(sourceFilePaths) do
+		self.sources[#self.sources+1] = sourceFilePath
+	end
+end
+
+function BuildTargetMixin:GetIncludeFlags()
+	local includeFlags = ""
+	for _, includeDir in ipairs(self.includeDirectories) do
+		includeFlags = includeFlags .. GCC_INCLUDE_FLAG .. includeDir .. " "
+	end
+	return includeFlags
+end
+
+return BuildTargetMixin
