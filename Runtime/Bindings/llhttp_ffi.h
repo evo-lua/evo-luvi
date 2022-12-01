6d5a5614eff66045b57e1a736e885ac20145ecb0
RDW <duckwhale@mail.ru>
2022-12-01 12:01:20 +0100

    Update llhttp_ffi.h

--- Runtime/Bindings/llhttp_ffi.h
+++ Runtime/Bindings/llhttp_ffi.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "llhttp.h"
 #include "lua.h"
 
 #include <stdint.h>
@@ -49,6 +50,29 @@ struct luajit_stringbuffer_reference {
 	size_t used;
 };
 typedef struct luajit_stringbuffer_reference luajit_stringbuffer_reference_t;
+
+// A thin wrapper for the llhttp API, only needed to expose the statically-linked llhttp symbols to Lua and load them via FFI
+struct static_llhttp_exports_table {
+	void (*llhttp_init)(llhttp_t* parser, llhttp_type_t type, const llhttp_settings_t* settings);
+	void (*llhttp_reset)(llhttp_t* parser);
+	void (*llhttp_settings_init)(llhttp_settings_t* settings);
+	llhttp_errno_t (*llhttp_execute)(llhttp_t* parser, const char* data, size_t len);
+	llhttp_errno_t (*llhttp_finish)(llhttp_t* parser);
+	int (*llhttp_message_needs_eof)(const llhttp_t* parser);
+	int (*llhttp_should_keep_alive)(const llhttp_t* parser);
+	void (*llhttp_pause)(llhttp_t* parser);
+	void (*llhttp_resume)(llhttp_t* parser);
+	void (*llhttp_resume_after_upgrade)(llhttp_t* parser);
+	llhttp_errno_t (*llhttp_get_errno)(const llhttp_t* parser);
+	const char* (*llhttp_get_error_reason)(const llhttp_t* parser);
+	void (*llhttp_set_error_reason)(llhttp_t* parser, const char* reason);
+	const char* (*llhttp_get_error_pos)(const llhttp_t* parser);
+	const char* (*llhttp_errno_name)(llhttp_errno_t err);
+	const char* (*llhttp_method_name)(llhttp_method_t method);
+	void (*llhttp_set_lenient_headers)(llhttp_t* parser, int enabled);
+	void (*llhttp_set_lenient_chunked_length)(llhttp_t* parser, int enabled);
+	void (*llhttp_set_lenient_keep_alive)(llhttp_t* parser, int enabled);
+};
 
 const char* llhttp_get_version_string(void);
 void export_llhttp_bindings(lua_State* L);
