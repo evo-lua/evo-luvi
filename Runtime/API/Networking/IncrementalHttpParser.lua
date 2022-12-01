c5544b094afb10db3d57c578b4b5e94e27d598d0
RDW <duckwhale@mail.ru>
2022-12-01 12:42:26 +0100

    Update IncrementalHttpParser.lua

--- Runtime/API/Networking/IncrementalHttpParser.lua
+++ Runtime/API/Networking/IncrementalHttpParser.lua
@@ -29,7 +29,7 @@ function IncrementalHttpParser:Construct()
 	-- To avoid C->Lua callbacks (which are extremely slow), buffer all events there... then replay them in Lua for fun and profit!
 	-- This effectively trades CPU time for memory and should be "OK" for all common use cases (20-50x speedup vs. 24 extra bytes/event)
 	instance.eventBuffer = string_buffer.new()
-	-- We can't easily pass the actual LuaJIT SBuf type to C, so use a proxy type for the writable area of the buffer instead
+	-- We can't easily pass the actual LuaJIT SBuf type to C, so use a proxy type to represent the writable area of the buffer instead
 	instance.state.data = ffi.new("luajit_stringbuffer_reference_t")
 
 	setmetatable(instance, { __index = self })
