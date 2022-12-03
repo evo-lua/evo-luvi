5c6a0caae288f6e8d33a30592f699cad27b87795
RDW <duckwhale@mail.ru>
2022-12-03 03:36:17 +0100

    Update IncrementalHttpParser.spec.lua

--- Tests/Runtime/API/Networking/IncrementalHttpParser.spec.lua
+++ Tests/Runtime/API/Networking/IncrementalHttpParser.spec.lua
@@ -1,6 +1,7 @@
 local IncrementalHttpParser = C_Networking.IncrementalHttpParser
 
 local llhttp = require("llhttp")
+local ffi = require("ffi")
 
 local websocketsRequestString =
 	"GET /chat HTTP/1.1\r\nHost: example.com:8000\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\nSec-WebSocket-Version: 13\r\n\r\n"
@@ -30,26 +31,22 @@ local nodeRequest2 = "POST /joyent/http-parser HTTP/1.1\r\n"
 	.. "Cache-Control: max-age=0\r\n\r\nb\r\nhello world\r\n0\r\n\r\n"
 
 describe("IncrementalHttpParser", function()
-	describe("Construct", function()
+		describe("Construct", function()
 		local parser = IncrementalHttpParser()
-		it("should initialize an empty event buffer", function()
-			local eventBuffer = parser:GetEventBuffer()
-
+		it("should initialize the parser with an empty event buffer", function()
 			assertEquals(parser:GetNumBufferedEvents(), 0)
-			assertEquals(tostring(eventBuffer), "")
-			assertEquals(#eventBuffer, 0)
-
+			assertEquals(parser:GetEventBufferSize(), 0)
 			assertEquals(parser:GetBufferedEvents(), {})
 		end)
 
 		-- it("should register event handlers for all llhttp-ffi events", function()
-		-- 	local llhttpEvents = llhttp.FFI_EVENTS
-		-- 	dump(parser)
-		-- 	for index, eventID in ipairs(llhttpEvents) do
-		-- 		assertTrue(parser:IsEventRegistered(eventID), eventID)
-		-- 	end
+			-- 	local llhttpEvents = llhttp.FFI_EVENTS
+			-- 	dump(parser)
+			-- 	for index, eventID in ipairs(llhttpEvents) do
+				-- 		assertTrue(parser:IsEventRegistered(eventID), eventID)
+				-- 	end
 
-		-- 	-- Zero-indexed by design, which means the fallback is skipped by ipairs
+				-- 	-- Zero-indexed by design, which means the fallback is skipped by ipairs
 		-- 	assertTrue(parser:IsEventRegistered("HTTP_EVENT_BUFFER_TOO_SMALL"), "HTTP_EVENT_BUFFER_TOO_SMALL")
 		-- end)
 
@@ -65,28 +62,57 @@ describe("IncrementalHttpParser", function()
 	end)
 
 	describe("ParseNextChunk", function()
-		it("should replay all buffered llhttp-ffi events in the order that they were queued in", function()
-			local parser = IncrementalHttpParser()
-			local chunk = nodeRequest2
-
-			parser:ParseNextChunk(chunk)
-
-
-			local eventBuffer = parser:GetEventBuffer()
-			assertEquals(parser:GetNumBufferedEvents(), 42)
-			assertEquals(tostring(eventBuffer), "test")
-			assertEquals(#eventBuffer, 42)
-
-			local events = parser:GetBufferedEvents()
-			dump(events)
-
-
-
-			-- print(type(parser.eventBuffer))
-			-- print(parser.state.data)
-
-			-- assertEquals(#parser.eventBuffer, #chunk)
-			-- assertEquals(tostring(parser.eventBuffer), chunk)
+		it("should add new lhttp-ffi events to the event buffer if any were triggered", function()
+				local parser = IncrementalHttpParser()
 		end)
+		-- it("should replay all buffered llhttp-ffi events in the order that they were queued in", function()
+		-- 	local parser = IncrementalHttpParser()
+		-- 	local chunk = nodeRequest2
+
+		-- 	parser:ParseNextChunk(chunk)
+
+
+		-- 	local eventBuffer = parser:GetEventBuffer()
+		-- 	assertEquals(parser:GetNumBufferedEvents(), 42)
+		-- 	assertEquals(tostring(eventBuffer), "test")
+		-- 	assertEquals(#eventBuffer, 42)
+
+		-- 	local events = parser:GetBufferedEvents()
+		-- 	dump(events)
+
+
+
+		-- 	-- print(type(parser.eventBuffer))
+		-- 	-- print(parser.state.data)
+
+		-- 	-- assertEquals(#parser.eventBuffer, #chunk)
+		-- 	-- assertEquals(tostring(parser.eventBuffer), chunk)
+		-- end)
+	end)
+
+	describe("GetNumBufferedEvents", function()
+		local parser = IncrementalHttpParser()
+		it("should return zero if no events have been buffered yet", function()
+			assertEquals(parser:GetEventBufferSize(), 0)
+		end)
+
+		it("should return the size of the event buffer if at least one event has been buffered", function()
+			local event = ffi.new("llhttp_event_t")
+			parser:AddBufferedEvent(event)
+			-- Since LuaJIT sizes the string buffer automatically, we don't "know" how much it will return
+			-- It's probably always the next power-of-two, but that's an implementation detail we'll ignore for now
+			-- The important part is that there's (more than) enough space for all events in the buffer at all times
+			assertTrue(parser:GetEventBufferSize() >= ffi.sizeof("llhttp_event_t"))
+		end)
+	end)
+
+	describe("GetEventBufferSize", function()
+	end)
+	describe("GetBufferedEvents", function()
+	end)
+	describe("ClearBufferedEvents", function()
+	end)
+	describe("ReplayBufferedEvents", function()
+
 	end)
 end)
