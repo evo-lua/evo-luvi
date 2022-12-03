d05bc81abb9cf89760f9f4d59aaf6338b5858e79
RDW <duckwhale@mail.ru>
2022-12-03 18:59:16 +0100

    Update IncrementalHttpParser.spec.lua

--- Tests/Runtime/API/Networking/IncrementalHttpParser.spec.lua
+++ Tests/Runtime/API/Networking/IncrementalHttpParser.spec.lua
@@ -5,29 +5,6 @@ local ffi = require("ffi")
 
 local websocketsRequestString =
     "GET /chat HTTP/1.1\r\nHost: example.com:8000\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\nSec-WebSocket-Version: 13\r\n\r\n"
-local nodeRequest1 =
-    "GET /wp-content/uploads/2010/03/hello-kitty-darth-vader-pink.jpg HTTP/1.1\r\n" ..
-        "Host: www.kittyhell.com\r\n" ..
-        "User-Agent: Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; ja-JP-mac; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 Pathtraq/0.9\r\n" ..
-        "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n" ..
-        "Accept-Language: ja,en-us;q=0.7,en;q=0.3\r\n" ..
-        "Accept-Encoding: gzip,deflate\r\n" ..
-        "Accept-Charset: Shift_JIS,utf-8;q=0.7,*;q=0.7\r\n" ..
-        "Keep-Alive: 115\r\n" .. "Connection: keep-alive\r\n" ..
-        "Cookie: wp_ozh_wsa_visits=2; wp_ozh_wsa_visit_lasttime=xxxxxxxxxx; __utma=xxxxxxxxx.xxxxxxxxxx.xxxxxxxxxx.xxxxxxxxxx.xxxxxxxxxx.x; __utmz=xxxxxxxxx.xxxxxxxxxx.x.x.utmccn=(referral)|utmcsr=reader.livedoor.com|utmcct=/reader/|utmcmd=referral\r\n\r\n"
-local nodeRequest2 = "POST /joyent/http-parser HTTP/1.1\r\n" ..
-                         "Host: github.com\r\n" .. "DNT: 1\r\n" ..
-                         "Accept-Encoding: gzip, deflate, sdch\r\n" ..
-                         "Accept-Language: ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4\r\n" ..
-                         "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) " ..
-                         "AppleWebKit/537.36 (KHTML, like Gecko) " ..
-                         "Chrome/39.0.2171.65 Safari/537.36\r\n" ..
-                         "Accept: text/html,application/xhtml+xml,application/xml;q=0.9," ..
-                         "image/webp,*/*;q=0.8\r\n" ..
-                         "Referer: https://github.com/joyent/http-parser\r\n" ..
-                         "Connection: keep-alive\r\n" ..
-                         "Transfer-Encoding: chunked\r\n" ..
-                         "Cache-Control: max-age=0\r\n\r\nb\r\nhello world\r\n0\r\n\r\n"
 
 describe("IncrementalHttpParser", function()
     describe("Construct", function()
@@ -236,6 +213,11 @@ describe("IncrementalHttpParser", function()
             end)
     end)
 
+	-- TODO
     describe("ClearBufferedEvents", function() end)
     describe("ReplayBufferedEvents", function() end)
+    describe("ReplayEvent", function() end)
+
+    describe("GetBufferedMessage", function() end)
+    describe("ResetBufferedMessage", function() end)
 end)
