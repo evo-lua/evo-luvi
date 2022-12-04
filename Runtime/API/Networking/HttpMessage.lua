4e2b2bec573e71874c9f44eda94cb50a142f2ded
RDW <duckwhale@mail.ru>
2022-12-04 02:09:02 +0100

    Update HttpMessage.lua

--- Runtime/API/Networking/HttpMessage.lua
+++ Runtime/API/Networking/HttpMessage.lua
@@ -1,12 +1,21 @@
+local string_buffer = require("string.buffer")
+local buffer_new = string_buffer.new
+
 -- Terminology used: See RFC 9112
-local HttpMessage = {
-	startLine = "", -- requestLine or statusLine
-	headerFieldLines = {},
-	messageBody = "",
-}
+local HttpMessage = {}
 
 function HttpMessage:Construct()
 	-- TODO reuse existing code
+
+	local instance = {
+		startLine = buffer_new(), -- requestLine or statusLine
+		headerFieldLines = {},
+		messageBody = buffer_new(),
+	}
+
+	setmetatable(instance, self)
+
+	return instance
 end
 
 function HttpMessage:IsRequest()
@@ -20,3 +29,10 @@ function HttpMessage:IsResponse()
 end
 
 function HttpMessage:IsStatusLine(buffer) end
+
+HttpMessage.__call = HttpMessage.Construct
+HttpMessage.__index = HttpMessage
+
+setmetatable(HttpMessage, HttpMessage)
+
+return HttpMessage
