17f6fa1942cb7f79257a59ac922e4c9c9e3985a9
RDW <duckwhale@mail.ru>
2022-12-22 21:34:27 +0100

    Update C_Networking.lua

--- Runtime/API/Networking/C_Networking.lua
+++ Runtime/API/Networking/C_Networking.lua
@@ -8,4 +8,46 @@ local C_Networking = {
 	AsyncSocketMixin = require("AsyncSocketMixin"),
 }
 
+local ffi = require("ffi")
+
+local ffi_cast = ffi.cast
+local ffi_sizeof = ffi.sizeof
+local table_insert = table.insert
+
+-- Whether or not these really belong here is highly questionable. Will have to revisit this later...
+function C_Networking.DecodeBufferAsArrayOf(stringBuffer, cType)
+	local bufferedEvents = {}
+
+	for index = 0, C_Networking.GetNumElementsOfType(stringBuffer, cType) - 1, 1 do
+		local event = C_Networking.DecodeElementAtBufferIndexAs(stringBuffer, index, cType)
+		table_insert(bufferedEvents, event)
+	end
+
+	return bufferedEvents
+end
+
+function C_Networking.GetNumElementsOfType(stringBuffer, cType)
+	return #stringBuffer / ffi_sizeof(cType)
+end
+
+function C_Networking.DecodeElementAtBufferIndexAs(stringBuffer, index, cType)
+	index = index or 0
+
+	if #stringBuffer == 0 then
+		return
+	end
+
+	local startPointer = stringBuffer:ref()
+	local offset = index * ffi_sizeof(cType)
+
+	-- TODO test or remove
+	-- local lastValidIndex  = self:GetNumBufferedEvents() - 1
+	-- if index < 0 or index > lastValidIndex then return nil end
+
+	-- TODO no GC anchor!
+	local event = ffi_cast("llhttp_event_t*", startPointer + offset)
+
+	return event
+end
+
 return C_Networking
