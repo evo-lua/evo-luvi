4902841b5971cdb9eb4b16b7a0ebd5f46fe3e5bc
RDW <duckwhale@mail.ru>
2022-12-27 15:14:30 +0100

    Runtime: Add HTTP parsing support to the Networking API

--- Runtime/API/Networking/C_Networking.lua
+++ Runtime/API/Networking/C_Networking.lua
@@ -2,6 +2,7 @@ local C_Networking = {
 	TcpSocket = require("TcpSocket"),
 	TcpServer = require("TcpServer"),
 	TcpClient = require("TcpClient"),
+	IncrementalHttpParser = require("IncrementalHttpParser"),
 	AsyncHandleMixin = require("AsyncHandleMixin"),
 	AsyncStreamMixin = require("AsyncStreamMixin"),
 	AsyncSocketMixin = require("AsyncSocketMixin"),
