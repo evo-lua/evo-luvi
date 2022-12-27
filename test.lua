9c8b3ae525df2101f78c79b7b052b79ad7ec46af
RDW <duckwhale@mail.ru>
2022-12-27 15:10:44 +0100

    Tests: Add unit tests for the IncrementalHttpParser builtin

--- test.lua
+++ test.lua
@@ -9,6 +9,7 @@ local testCases = {
 	"Tests/Runtime/API/FileSystem/C_FileSystem.spec.lua",
 	"Tests/Runtime/API/Testing/Scenario.spec.lua",
 	"Tests/Runtime/API/Testing/TestSuite.spec.lua",
+	"Tests/Runtime/API/Networking/IncrementalHttpParser.spec.lua",
 	"Tests/Runtime/API/Networking/C_Networking.spec.lua",
 	"Tests/Primitives/extend.spec.lua",
 	"Tests/Primitives/format.spec.lua",
