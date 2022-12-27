af3ef9fdd86917a63972bca7b43eaa53e894e952
RDW <duckwhale@mail.ru>
2022-12-27 15:06:12 +0100

    Repo: Add some basic benchmarks for the HTTP parser

---
+++ Benchmarks/Benchmark.lua
@@ -0,0 +1,47 @@
+local uv = require("uv")
+local bold = transform.bold
+
+local Benchmark = {
+	label = "<no label>",
+	iterationCount = 10000000,
+}
+
+function Benchmark:Construct(label)
+	local instance = {}
+
+	if type(label) == "string" then
+		instance.label = label
+	end
+
+	setmetatable(instance, self)
+
+	return instance
+end
+
+Benchmark.__index = Benchmark
+Benchmark.__call = Benchmark.Construct
+setmetatable(Benchmark, Benchmark)
+
+function Benchmark:Start(numIterations)
+	self:OnSetup()
+
+	numIterations = numIterations or self.iterationCount
+	print()
+	printf("Starting benchmark %s for %d iterations ", bold(self.label), numIterations)
+
+	local startTime = uv.hrtime()
+	for i = 1, numIterations, 1 do
+		self:OnRun()
+	end
+	local endTime = uv.hrtime()
+
+	self:OnReport(endTime - startTime, numIterations)
+
+	return true
+end
+
+function Benchmark:OnSetup() end
+function Benchmark:OnRun() end
+function Benchmark:OnReport() end
+
+return Benchmark
