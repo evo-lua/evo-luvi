425df459e08fa99691216b381c38dade691d8bef
RDW <duckwhale@mail.ru>
2022-12-29 15:50:50 +0100

    Update url.spec.lua

--- Tests/Extensions/url.spec.lua
+++ Tests/Extensions/url.spec.lua
@@ -3,3 +3,47 @@ describe("url", function()
 
 	error("nyi")
 end)
+
+-- Class: URL
+-- new URL(input[, base])
+-- url.hash
+-- url.host
+-- url.hostname
+-- url.href
+-- url.origin
+-- url.password
+-- url.pathname
+-- url.port
+-- url.protocol
+-- 	Special schemes
+-- url.search
+-- url.searchParams
+-- url.username
+-- url.toString()
+-- url.toJSON()
+-- URL.createObjectURL(blob)
+-- URL.revokeObjectURL(id)
+-- Class: URLSearchParams
+-- new URLSearchParams()
+-- new URLSearchParams(string)
+-- new URLSearchParams(obj)
+-- new URLSearchParams(iterable)
+-- urlSearchParams.append(name, value)
+-- urlSearchParams.delete(name)
+-- urlSearchParams.entries()
+-- urlSearchParams.forEach(fn[, thisArg])
+-- urlSearchParams.get(name)
+-- urlSearchParams.getAll(name)
+-- urlSearchParams.has(name)
+-- urlSearchParams.keys()
+-- urlSearchParams.set(name, value)
+-- urlSearchParams.sort()
+-- urlSearchParams.toString()
+-- urlSearchParams.values()
+-- urlSearchParams[Symbol.iterator]()
+-- url.domainToASCII(domain)
+-- url.domainToUnicode(domain)
+-- url.fileURLToPath(url)
+-- url.format(URL[, options])
+-- url.pathToFileURL(path)
+-- url.urlToHttpOptions(url)
